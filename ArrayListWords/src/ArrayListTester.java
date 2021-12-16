3d54dab38dcd6439bba209f9e3214117df93fb6f
Jarrett Nobles <noblesjarrett035@gmail.com>
2021-12-15 19:17:44 -0500

    Update ArrayListTester.java
    Closed the Scanner input

--- ArrayListWords/src/ArrayListTester.java
+++ ArrayListWords/src/ArrayListTester.java
@@ -4,14 +4,15 @@ public class ArrayListTester
 
 	public static void main(String[] args) 
 	{
-		//get number of words the array list will contain
-		Scanner in = new Scanner(System.in);
-		int num;
-		System.out.println("Enter a number of words: ");
-		num=in.nextInt();
-		//construct an object of the array list words class
-		ArrayListWords myWords = new ArrayListWords(num);
-		myWords.run();
+		try (//get number of words the array list will contain
+		Scanner in = new Scanner(System.in)) {
+			int num;
+			System.out.println("Enter a number of words: ");
+			num=in.nextInt();
+			//construct an object of the array list words class
+			ArrayListWords myWords = new ArrayListWords(num);
+			myWords.run();
+		}
 		System.out.println("test");
 	}
 
